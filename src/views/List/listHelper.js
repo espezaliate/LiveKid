1b209ff1db91a67a0f7376b65e6797226787b04b
espezaliate <mkoziol54@gmail.com>
2022-01-27 22:50:59 +0100

    Fixed search bar to filter by names, status and tags

--- src/views/List/listHelper.js
+++ src/views/List/listHelper.js
@@ -1,11 +1,16 @@
 export const filterList = (item, search) => {
-   const keysToFilter = ['name', 'status', 'img', 'tags']
+   const keysToFilter = ['name', 'status', 'tags']
+
    return Object.entries(item).some(
       ([key, value]) =>
-         keysToFilter.includes(key) && value.toLowerCase().includes(search)
+         keysToFilter.includes(key) &&
+         String(value)
+            .toLowerCase()
+            .includes(search)
    )
 }
 export const mapList = item => {
+   console.log(item)
    const tags_display = item.tags.join(', ')
    const cords_display = item.cords.join(', ')
    return { ...item, tags_display, cords_display }
